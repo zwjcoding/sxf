1609c083353c25e3ec18cfb18082ea6d56392947
zhaowenju <zhaowenju0414@gmail.com>
2018-08-13 13:54:59 +0800

    login

--- src/main/java/com/pos/cn/sxf/controller/LoginController.java
+++ src/main/java/com/pos/cn/sxf/controller/LoginController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
 
+    @RequestMapping(value = "login")
+       public  String  loginUser(){
+                return "login.jsp";
+       }
 
 
 
